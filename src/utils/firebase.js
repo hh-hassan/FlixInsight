79da47e5bd40a1aa383125c2d62c2d4805e3031d
Hassanul Haque <hassaniitkgp@gmail.com>
2024-09-23 16:53:25 +0530

    edit

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCL-Y8Lq1qnr0TcuqPwjGNrLKyModRPG64",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "flixinsight-d94ca.firebaseapp.com",
   projectId: "flixinsight-d94ca",
   storageBucket: "flixinsight-d94ca.appspot.com",
